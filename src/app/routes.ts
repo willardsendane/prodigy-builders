6a5eb910dd09ee8444972e54b6d3be0c213023b1
willardsendane <willardsendane@gmail.com>
2026-03-26 20:30:27 -0600

    Added Foremaster project page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { OurWork } from "./pages/OurWork";
+import { ProjectForemaster } from "./pages/ProjectForemaster";
 import { ClientDropbox } from "./pages/ClientDropbox";
 import { Contact } from "./pages/Contact";
 import { Layout } from "./components/Layout";
@@ -14,6 +15,7 @@ export const router = createBrowserRouter([
       { index: true, Component: Home },
       { path: "about", Component: About },
       { path: "work", Component: OurWork },
+      { path: "work/foremaster", Component: ProjectForemaster },
       { path: "dropbox", Component: ClientDropbox },
       { path: "contact", Component: Contact },
     ],
